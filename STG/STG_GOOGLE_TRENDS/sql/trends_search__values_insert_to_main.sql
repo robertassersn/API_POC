e9e70f0122c4a6fd96533d68ed13e47bc22aa492
sersniovas <38590951+sersniovas@users.noreply.github.com>
2026-01-18 16:35:16 +0200

    .

--- STG/STG_GOOGLE_TRENDS/sql/trends_search__values_insert_to_main.sql
+++ STG/STG_GOOGLE_TRENDS/sql/trends_search__values_insert_to_main.sql
@@ -1,22 +1,26 @@
 delete from ${SCHEMA_GOOGLE_TRENDS}.trends_search__values
 where 
-    google_trends_id in(
+    google_trends__values_id in(
         select 
-            google_trends_id
+            google_trends__values_id
         from ${SCHEMA_GOOGLE_TRENDS_TEMP}.trends_search__values_temp
     )
 ;
 insert into ${SCHEMA_GOOGLE_TRENDS}.trends_search__values(
-    google_trends_id
-	,date 
-	,timestamp
+    query 
+	,value 
+	,extracted_value 
+	,google_trends_id 
+	,google_trends__values_id
 	,source_file_name
 	,job_run_id
 )
 select 
-    google_trends_id
-	,"date"
-	,timestamp 
+    query 
+	,value 
+	,extracted_value 
+	,google_trends_id 
+	,google_trends__values_id
 	,source_file_name
 	,${JOB_RUN_ID} as job_run_id
 from ${SCHEMA_GOOGLE_TRENDS_TEMP}.trends_search__values_temp
