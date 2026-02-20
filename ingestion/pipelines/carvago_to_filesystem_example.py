7fb268339e0f4f83330bec915e473df518dc432f
sersniovas <38590951+sersniovas@users.noreply.github.com>
2026-02-20 08:05:05 +0200

    .

--- ingestion/pipelines/carvago_to_filesystem_example.py
+++ ingestion/pipelines/carvago_to_filesystem_example.py
@@ -3,21 +3,25 @@ import dlt
 import sys
 import os
 import logging
-
-# dlt.config["runtime.log_level"] = "WARNING"
 base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
 sys.path.append(base_path)
 from project_files import functions
 from ingestion.sources.carvago import carvago_source
 import logging
 from datetime import datetime
-
-config_dictionary = functions.read_config_segment()
-destination_raw = dlt.destinations.filesystem(bucket_url=config_dictionary['CARVAGO_DIR_DOWNLOADED_FILES'])
 os.environ["RUNTIME__LOG_LEVEL"] = "INFO"  # set before pipeline creation
 
-@functions.with_logging(log_dir="ingestion/pipelines/logs", level=logging.INFO)
-def run_pipeline():
+DATASOURCE = 'CARVAGO'
+PIPELINE_NAME = 'carvago_to_filesystem'
+
+config_dictionary = functions.read_config_segment(segment=DATASOURCE)
+destination_raw = dlt.destinations.filesystem(bucket_url=config_dictionary['DIR_DOWNLOADED_FILES'])
+
+@functions.with_logging(
+        log_dir=config_dictionary['PIPELINE_LOGS_DIR']
+        , level=logging.INFO
+    )
+def run_carvago_pipeline():
     logger = logging.getLogger(__name__) 
     dlt_logger = logging.getLogger("dlt")
     dlt_logger.propagate = True
@@ -25,14 +29,14 @@ def run_pipeline():
     # Copy handlers from root logger to dlt logger if propagation alone doesn't work
     for handler in logging.getLogger().handlers:
         dlt_logger.addHandler(handler)
-    logger.info("Starting pipeline run")
+    logger.info(f"datasource: {DATASOURCE} \n pipeline: {PIPELINE_NAME} \n EVENT: starting pipeline run" )
     pipeline = dlt.pipeline(
-        pipeline_name="carvago_to_filesystem",
-        export_schema_path="ingestion/schemas/export",
+        pipeline_name=PIPELINE_NAME,
+        export_schema_path=config_dictionary['DLT_SOURCE_SCHEMA_DIR'],
         destination=destination_raw,
-        dataset_name="carvago"
+        dataset_name=DATASOURCE
     )
-    logger.info("Pipeline defined, starting data extraction")
+    logger.info("datasource: {DATASOURCE}\n pipeline: {PIPELINE_NAME} \n EVENT: starting data extraction")
     pipeline.run(
         carvago_source(
             job_id=str(1),
@@ -48,8 +52,8 @@ def run_pipeline():
         )
         , write_disposition="replace"
     )
-    logger.info("Pipeline run complete")
+    logger.info("datasource: {DATASOURCE} \n pipeline: {PIPELINE_NAME} \n EVENT: pipeline run complete")
 
 
 if __name__ == "__main__":
-    run_pipeline()
+    run_carvago_pipeline()
