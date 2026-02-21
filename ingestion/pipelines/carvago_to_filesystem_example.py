f98592c2a82053f3f86b57451d0cac61293378ff
sersniovas <38590951+sersniovas@users.noreply.github.com>
2026-02-21 10:44:59 +0200

    .

--- ingestion/pipelines/carvago_to_filesystem_example.py
+++ ingestion/pipelines/carvago_to_filesystem_example.py
@@ -17,20 +17,17 @@ config_dictionary = functions.read_config_segment(segment=DATASOURCE)
 PIPELINE_RUN_PARAMETERS = json.loads(
     config_dictionary['RUN_CONFIGS']
     )
-
-destination_raw = dlt.destinations.filesystem(bucket_url=config_dictionary['DIR_DOWNLOADED_FILES'])
-
+destination = dlt.destinations.filesystem(bucket_url=config_dictionary['DIR_DOWNLOADED_FILES'])
 
 pipeline = run_dlt_pipeline(
     pipeline_name=PIPELINE_NAME,
     source_func=lambda **p: carvago_source(job_id=1, **p),
     run_parameters=PIPELINE_RUN_PARAMETERS,
-    destination=destination_raw,
+    destination=destination,
     dataset_name=DATASOURCE,
     export_schema_path=config_dictionary['DLT_SOURCE_SCHEMA_DIR'],
     log_dir=config_dictionary['DLT_PIPELINE_LOGS_DIR'],
 )
-
 
 if __name__ == "__main__":
     pipeline
